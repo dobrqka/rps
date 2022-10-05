71a93830746f10f243f37f976de8a2337b4760b7
dobrqka <dobrin.andreev@gmail.com>
2022-10-05 17:39:33 +0300

    Add game function
    Created a function that calls the single round function until
    one of the players has reached the score of 3 thus winning.

--- script.js
+++ script.js
@@ -31,39 +31,65 @@ function getPlayerChoice () {
 
 let computerSelection = getComputerChoice();
 let playerSelection = getPlayerChoice();
+let playerResult = 0;
+let computerResult = 0;
 
 // plays a single round
 
-function singleRound (playerSelection, computerSelection) {
+function playRound (playerSelection, computerSelection) {
     if (computerSelection == playerSelection) {
         alert("It's a tie!")
         return;
     }
     else if (computerSelection == 'ROCK' && playerSelection == 'PAPER') {
         alert("You win! The Rock can't resist them paper$!");
-        return;
-    }
+        return playerResult += 1;
+    } 
     else if (computerSelection == 'ROCK' && playerSelection == 'SCISSORS') {
         alert("Looooser! The Rock don't scissor no one!")
-        return;
+        return computerResult += 1;
     }
     else if (computerSelection == 'PAPER' && playerSelection == 'SCISSORS') {
         alert("You win! The best scissoring in life can't be bought with papers!")
-        return;
+        return playerResult += 1;
     }
     else if (computerSelection == 'PAPER' && playerSelection == 'ROCK') {
         alert("Looooser! The Rock will always fall victim to the papers' bidding!")
-        return;
+        return computerResult += 1;
     }
     else if (computerSelection == 'SCISSORS' && playerSelection == 'ROCK') {
         alert("You win mate! The Rock don't use no scissors!");
-        return;
+        return playerResult += 1;
     }
     else if (computerSelection == 'SCISSORS' && playerSelection == 'PAPER') {
         alert('Looooser! Them papers don\'t mean nothing if you know how to sccissor!')
-        return;
+        return computerResult += 1;
     }
 
 }
-// record the result and start another round
-// if the player or the computer reaches 3 - they win
+
+// game function that calls the playRound function and records the results until someone wins - THIS ONE IS STILL VERY MUCH OFF!    
+
+function game() {
+    playRound(playerSelection, computerSelection);
+    if (playerResult == 3) {
+        alert(`That's it, you win! 
+        The overall score is: 
+        You: ${playerResult} Robot: ${computerResult}`);
+        return;
+    }
+    else if (computerResult == 3) {
+        alert(`Sorry booooi! U a loser! 
+        The overall score is: 
+        You: ${playerResult} Robot: ${computerResult}`);
+        return;
+    }
+    else { 
+        alert(`Score:
+        You: ${playerResult} Robot: ${computerResult}`)
+        computerSelection = getComputerChoice();
+        playerSelection = getPlayerChoice();
+        game() }
+}
+
+game();
