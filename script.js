6ce09381c4fa4a2dc5540b533ceef27232098f04
dobrqka <dobrin.andreev@gmail.com>
2022-11-30 17:36:49 +0200

    Change classic game to a custom one
    Changed the rock, paper and scissors options to the custom ones

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const computerResult = document.querySelector('.computerResult');
 // get a random computer selection
 
 function computerSelection() {
-  const computerChoices = ["ROCK", "PAPER", "SCISSORS"];
+  const computerChoices = ["bomb", "cockroach", "foot"];
   let chosenOne =     computerChoices[Math.floor(Math.random()*computerChoices.length)];
   return chosenOne;
 }
@@ -23,14 +23,14 @@ function playRound (playerSelection, computerSelection) {
         alert("It's a tie!");
         return;
     }
-    else if ((computerSelection == 'ROCK' && playerSelection == 'PAPER') || (computerSelection == 'PAPER' && playerSelection == 'SCISSORS') || (computerSelection == 'SCISSORS' && playerSelection == 'ROCK')) {
+    else if ((computerSelection == 'bomb' && playerSelection == 'cockroach') || (computerSelection == 'cockroach' && playerSelection == 'foot') || (computerSelection == 'foot' && playerSelection == 'bomb')) {
       alert('You win!');
       playerScore += 1;
       playerResult.textContent = `Player Score: ${playerScore}`;
       if (playerScore == 5) { alert('You are the new Champion!')}
       else return;
     } 
-    else if ((computerSelection == 'ROCK' && playerSelection == 'SCISSORS') || (computerSelection == 'PAPER' && playerSelection == 'ROCK') || (computerSelection == 'SCISSORS' && playerSelection == 'PAPER')) {
+    else if ((computerSelection == 'bomb' && playerSelection == 'foot') || (computerSelection == 'cockroach' && playerSelection == 'bomb') || (computerSelection == 'foot' && playerSelection == 'cockroach')) {
       alert('You lose.');
       computerScore += 1;
       computerResult.textContent = `Computer Score: ${computerScore}`;
@@ -43,5 +43,17 @@ function playRound (playerSelection, computerSelection) {
 
 const theButtons = document.querySelectorAll('.buttons button');
 for (i=0; i<theButtons.length; i++) {
- theButtons[i].addEventListener('click', (e) =>  {playRound(e.target.textContent, computerSelection())})
+ theButtons[i].addEventListener('click', (e) =>  {playRound(e.target.className, computerSelection())})
 }
+
+// create a button that shows the rules
+
+const rules = document.createElement('button');
+rules.textContent = 'Rules';
+document.body.appendChild(rules);
+
+function showRules () {
+  alert("This is just like the classic game but with a twist. Here are the rules: Atomic Bomb makes Foot explode, Foot squashes Cockroach and Cockroach survives Atomic Bomb! Have fun and may the force be with you!");
+}
+
+rules.addEventListener('click', showRules);
